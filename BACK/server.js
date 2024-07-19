844491adceb8564438f976e549c3aee2b6ad2c21
Mathias Jacquart <math.jacquart@gmail.com>
2024-07-19 14:51:08 +0200

    v-5

--- BACK/server.js
+++ BACK/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
-const path = require("path")
+const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
