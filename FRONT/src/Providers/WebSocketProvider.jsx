7fe31a75db36ad623d847caef6f06b7a1514d2d6
Mathias JACQUART <150044178+mathiasjacquart@users.noreply.github.com>
2025-05-19 09:21:48 +0200

    wbsocket adresss

--- FRONT/src/Providers/WebSocketProvider.jsx
+++ FRONT/src/Providers/WebSocketProvider.jsx
@@ -5,8 +5,8 @@ export const WebSocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    // const newSocket = new WebSocket('https://quizz-musical.onrender.com');
-    const newSocket = new WebSocket("ws://localhost:8080");
+    const newSocket = new WebSocket("https://quizz-musical.onrender.com");
+    // const newSocket = new WebSocket("ws://localhost:8080");
     setSocket(newSocket);
 
     return () => newSocket.close();
